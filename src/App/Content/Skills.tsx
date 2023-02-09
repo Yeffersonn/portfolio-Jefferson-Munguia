b8bcdab9340affcc62f196cfb1990a45414b42cb
Jefferson Munguia <jefferson.munguia.egusquiza@gmail.com>
2023-02-09 11:54:57 -0500

    Skills Updated

--- src/App/Content/Skills.tsx
+++ src/App/Content/Skills.tsx
@@ -1,11 +1,24 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import JavaScript from "../assets/img/javascript.svg";
+import TypeScript from "../assets/img/typescript.svg";
+import React from "../assets/img/react.svg";
+import Redux from "../assets/img/redux.svg";
+import Next from "../assets/img/next.svg";
+import HTML from "../assets/img/html.svg";
+import CSS from "../assets/img/css.svg";
+import Bootstrap from "../assets/img/bootstrap.svg";
+import Tailwind from "../assets/img/tailwind.svg";
+import Sass from "../assets/img/sass.svg";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png";
+
+
+export interface skill {
+	img: string,
+	name: string,
+}
 
 export const Skills = () => {
 	const responsive = {
@@ -28,6 +41,19 @@ export const Skills = () => {
 		},
 	};
 
+	const skills:skill[] = [
+		{ img: JavaScript, name: "JavaScript" },
+		{ img: TypeScript, name: "TypeScript" },
+		{ img: React, name: "React" },
+		{ img: Redux, name: "Redux" },
+		{ img: Next, name: "NextJS" },
+		{ img: HTML, name: "HTML" },
+		{ img: CSS, name: "CSS" },
+		{ img: Bootstrap, name: "Bootstrap" },
+		{ img: Tailwind, name: "Tailwind CSS" },
+		{ img: Sass, name: "Sass" },
+	];
+
 	return (
 		<section className="skill" id="skills">
 			<div className="container">
@@ -36,41 +62,28 @@ export const Skills = () => {
 						<div className="skill-bx wow zoomIn">
 							<h2>Skills</h2>
 							<p>
-								In this section you will find all the
-								programming languages <br/> ​​that I use as a frontend
-								developer.
+								In this section you will find all the programming
+								languages <br /> ​​that I use as a frontend developer.
 							</p>
 							<Carousel
 								responsive={responsive}
 								infinite={true}
 								className="owl-carousel owl-theme skill-slider"
 							>
-								<div className="item">
-									<img src={meter1} alt="Image" />
-									<h5>Web Development</h5>
-								</div>
-								<div className="item">
-									<img src={meter2} alt="Image" />
-									<h5>Brand Identity</h5>
-								</div>
-								<div className="item">
-									<img src={meter3} alt="Image" />
-									<h5>Logo Design</h5>
-								</div>
-								<div className="item">
-									<img src={meter1} alt="Image" />
-									<h5>Web Development</h5>
-								</div>
+								{skills.map(({img,name}) => {
+									return (
+										<div className="item" key={name}>
+											<img src={img} alt={name} />
+											<h5>{name}</h5>
+										</div>
+									);
+								})}
 							</Carousel>
 						</div>
 					</div>
 				</div>
 			</div>
-			<img
-				className="background-image-left"
-				src={colorSharp}
-				alt="Image"
-			/>
+			<img className="background-image-left" src={colorSharp} alt="Image" />
 		</section>
 	);
 };
